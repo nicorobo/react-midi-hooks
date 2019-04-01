529cf55934e0dca9a4ae6c708918a5737c2ebc4e
nickroberts404 <nickroberts404@gmail.com>
2019-04-01 01:07:52 -0500

    Initialized project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js',
+		libraryTarget: 'commonjs2',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
+				exclude: /(node_modules|build)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	externals: {
+		react: 'commonjs react',
+	},
+};
