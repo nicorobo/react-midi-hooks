536502c3d3397e927084d2c309289b7875cc4f05
Nick Roberts <nick@labelbox.com>
2019-12-05 21:19:17 -0600

    Added check for navigator; removed internal midi clock (will build better one)

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,27 @@
-var path = require('path');
+var path = require("path");
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'index.js',
-		libraryTarget: 'commonjs2',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				include: path.resolve(__dirname, 'src'),
-				exclude: /(node_modules|build)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-			{
-				test: /\.worker\.js$/,
-				use: {
-					loader: 'worker-loader',
-					options: {
-						inline: true,
-					},
-				},
-			},
-		],
-	},
-	externals: {
-		react: 'commonjs react',
-	},
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    react: "commonjs react"
+  }
 };
