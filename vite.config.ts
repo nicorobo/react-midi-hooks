849f05d8b6ff9a3d6e8e1ab31d9598b4d0c2cf82
nicorobo <nickroberts404@gmail.com>
2024-02-14 13:09:45 -0800

    updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +9,11 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
+      fileName: 'main',
       formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
     },
   },
-});
+})
